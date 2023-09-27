cf4427e972cc03e406abd1b19f546b5ff1f55585
“Eltion <ebehrami01@gmail.com>
2023-09-27 14:28:25 -0400

    phase II complete

---
+++ FirstRoutes/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def index
+        # users = User.all
+        # render json: users
+        render plain: "I'm in the index action!"
+    end 
+
+    def create
+
+    end 
+
+
+    def show 
+
+    end 
+
+    def update
+
+    end 
+
+    def destroy 
+
+    end 
+
+end
